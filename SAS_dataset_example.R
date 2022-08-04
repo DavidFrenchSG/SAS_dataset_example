f7cb0eae90a6625ad968d993d3d747e3d838ebfe
David French <David.French@gov.scot>
2022-08-04 19:00:25 +0100

    Initial commit

---
+++ SAS_dataset_example.R
@@ -0,0 +1,27 @@
+#Very short example of R code to access SAS datasets, and output the weighted FBI
+
+
+library(tidyverse)
+library(haven)
+
+datayear=2021
+sampyear=2020
+
+setwd('//s0177a/sasdata1/ags/fas')
+# list.files()
+inputfile=paste("so_y",datayear,"_fa.sas7bdat",sep='')
+# inputfile
+FarmAccount <- read_sas(inputfile)
+names(FarmAccount) <- tolower(names(FarmAccount))
+FarmAccount <- FarmAccount %>% 
+  select(fa_id,type,fa_ifuel,fa_fbi)
+weights <- read_sas("new_weights.sas7bdat")
+names(weights)<-tolower(names(weights))
+MergedData <- FarmAccount%>% 
+  merge(weights,by="fa_id")
+MergedData <- MergedData %>% 
+  mutate(ys_year=(fa_id%%10000)) %>% 
+  filter(ys_year==sampyear)
+# View(MergedData)
+mean(MergedData$fa_fbi)
+weighted.mean(MergedData$fa_fbi,MergedData$fbswt)
